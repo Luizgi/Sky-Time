9c6802154b06a3e3dfbabd9a35e0edad8441d9ab
Luiz Guilherme de Souza Gonçalves <103260099+Luizgi@users.noreply.github.com>
2023-06-15 20:52:25 -0300

    update

---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhaseManager : MonoBehaviour
+{
+    public int ActualPhase;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if(ActualPhase > 0 && ActualPhase < 7)
+        {
+            PlayerPrefs.SetInt("FASE", ActualPhase);
+        }
+    }
+
+    // Update is called once per frame
+    public void Fase(int numberPhase)
+    {
+        SceneManager.LoadScene(numberPhase);
+    }
+
+    public void PassPhase()
+    {
+        int pPhase = ActualPhase + 1;
+        SceneManager.LoadScene(pPhase);
+    }
+
+    public void GoToSavedPhase()
+    {
+        int pSave = PlayerPrefs.GetInt("Fase");
+        SceneManager.LoadScene(pSave);
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(ActualPhase);
+    }
+
+    public void GoToStart()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
